bd51cb99b8f5c0b93028d9210e8dc3725e9e4883
happihound <36242570+happihound@users.noreply.github.com>
2022-10-07 15:34:25 -0400

    inital

---
+++ drawVectors.py
@@ -0,0 +1,52 @@
+import turtle
+import numpy as np
+from tqdm import tqdm
+myTurtle = turtle.Turtle()
+screen = turtle.Screen()
+screen.tracer(100000, 0)
+screen.colormode(255)
+myTurtle.ht()
+
+
+def runme():
+    printInstructions = loadData()
+    oldx = 0
+    oldy = 0
+    scaleFactor = 0.5
+    for y1, x1, y2, x2, color in tqdm(printInstructions):
+        y1 = y1 * scaleFactor
+        x1 = x1 * scaleFactor
+        y2 = y2 * scaleFactor
+        x2 = x2 * scaleFactor
+
+        myTurtle.pencolor(color)
+        if (abs(oldx-x1)+abs(oldy-y1)) > 5:
+            myTurtle.up()
+            myTurtle.setpos(x1, y1)
+            myTurtle.down()
+            myTurtle.setpos(x2, y2)
+            oldx = x2
+            oldy = y2
+            continue
+        myTurtle.setpos(x1, y1)
+        myTurtle.setpos(x2, y2)
+        oldx = x2
+        oldy = y2
+    screen.update()
+
+
+def loadData():
+    pathToData = 'outputData/polygonalPoints.npy'
+    a = np.load(pathToData, allow_pickle=True)
+    return a
+
+
+def main():
+    screen.screensize(2000, 2000)
+    screen.bgcolor("black")
+    myTurtle.width(2)
+    runme()
+    input("press enter to quit")
+
+
+main()
---
+++ imageToVectors.py
@@ -0,0 +1,73 @@
+import numpy as np
+from skimage.measure import approximate_polygon, find_contours
+import cv2 as cv
+import cv2
+from tqdm import tqdm
+
+
+def main():
+    lineResult = 0
+    # include abolute path to image
+    pictureName = ''
+    img1 = cv2.imread(pictureName)
+    img = cv2.imread(pictureName, 0)
+    scale_percent = 100  # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    # resize image
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+    img1 = cv2.resize(img1, dim, interpolation=cv2.INTER_AREA)
+    img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+                               cv.THRESH_BINARY, 11, 0)
+    img1 = cv2.flip(img1, -1)
+    img1 = cv2.rotate(img1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    img = cv2.flip(img, 0)
+    contours = find_contours(img, 4)
+    lineResult = np.zeros(img.shape + (3,), np.uint8)
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+    color = []
+    for con in tqdm(contours):
+        cv_contour = []
+        for point in con:
+            intify = [int(point[0]), int(point[1])]
+            cv_contour.append([intify])
+        cv_contour = np.array(cv_contour)
+        color.append(findColor(img1, [cv_contour.astype(int)]))
+    iterationCount = 0
+    points = []
+    for contour in tqdm(contours):
+        # change tolerance value to control quality
+        simplifiedLine = approximate_polygon(contour, tolerance=0)
+        simplifiedLine = simplifiedLine.astype(np.int64).tolist()
+        for index, coords in enumerate(simplifiedLine[:-1]):
+            y1, x1, y2, x2 = coords + simplifiedLine[index + 1]
+            if (x1 == x2 and y1 == y2):
+                continue
+            points.append(((y1 - 400), (x1 - 400), (y2 - 400), (x2 - 400), color[iterationCount]))
+            lineResult = cv2.line(lineResult, (x1, y1),
+                                  (x2, y2), color[iterationCount])
+        iterationCount = iterationCount + 1
+    save(points)
+    lineResult = cv2.flip(lineResult, 0)
+    lineResult = cv2.cvtColor(lineResult, cv2.COLOR_BGR2RGB)
+    cv2.imshow("title",  lineResult)
+    cv2.waitKey()
+
+
+def save(saved_points):
+    saved_points = np.array(saved_points, dtype=object)
+    np.save('outputdata/polygonalPoints.npy',
+            saved_points)
+
+
+def findColor(image, c):
+    mask = np.zeros(image.shape[:2], dtype="uint8")
+    cv2.drawContours(mask, c, -1, 255, -1)
+    mean = cv2.mean(image, mask=mask)[:3]
+    tupint = tuple(int(x) for x in mean)
+    return tupint
+
+
+main()
