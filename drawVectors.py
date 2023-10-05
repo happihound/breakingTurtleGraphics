54f88fc31217e1253998a504c2a0f07376130313
happihound <bevanneib@gmail.com>
2023-10-05 15:09:16 -0400

    updated the code, works better now

--- drawVectors.py
+++ drawVectors.py
@@ -2,38 +2,50 @@ import turtle
 import numpy as np
 from tqdm import tqdm
 
+
 def startDrawing():
     myTurtle = turtle.Turtle()
     screen = turtle.Screen()
-    screen.tracer(100000, 0)
     screen.colormode(255)
-    myTurtle.ht()
-    screen.screensize(2000, 2000)
+    # myTurtle.ht()
+    screen_size = (2000, 2000)
+    screen.screensize(screen_size[0], screen_size[1])
     screen.bgcolor("black")
     myTurtle.width(2)
     printInstructions = loadData()
+    screen.tracer(len(printInstructions)/100, 0)
     oldx = 0
     oldy = 0
+    old_color = (0, 0, 0)
+    myTurtle.up()
     scaleFactor = 0.5
-    for y1, x1, y2, x2, color in tqdm(printInstructions):
-        y1 = y1 * scaleFactor
-        x1 = x1 * scaleFactor
-        y2 = y2 * scaleFactor
-        x2 = x2 * scaleFactor
-
-        myTurtle.pencolor(color)
-        if (abs(oldx-x1)+abs(oldy-y1)) > 5:
+    x_offset = -500
+    y_offset = 200
+    for (x1, y1), (x2, y2), color in tqdm(printInstructions):
+        y1 = -y1 * scaleFactor + y_offset
+        x1 = x1 * scaleFactor + x_offset
+        y2 = -y2 * scaleFactor + y_offset
+        x2 = x2 * scaleFactor + x_offset
+        if color != old_color:
+            myTurtle.pencolor(color)
+        # If the distance between the last point and the current point is greater 5, then pick up the pen and move to the new point
+        distance_from_last_coord = np.sqrt((x1 - oldx)**2 + (y1 - oldy)**2)
+        if distance_from_last_coord > 5:
+            myTurtle.width(2)
             myTurtle.up()
             myTurtle.setpos(x1, y1)
             myTurtle.down()
             myTurtle.setpos(x2, y2)
             oldx = x2
             oldy = y2
+            old_color = color
             continue
+        myTurtle.width(3)
         myTurtle.setpos(x1, y1)
         myTurtle.setpos(x2, y2)
         oldx = x2
         oldy = y2
+        old_color = color
     screen.update()
     screen.exitonclick()
 
@@ -43,3 +55,6 @@ def loadData():
     a = np.load(pathToData, allow_pickle=True)
     return a
 
+
+if __name__ == "__main__":
+    startDrawing()
--- imageToVectors.py
+++ imageToVectors.py
@@ -1,19 +1,19 @@
 import numpy as np
-from skimage.measure import approximate_polygon, find_contours
-import cv2 as cv
 import cv2
 from tqdm import tqdm
+from skimage.measure import approximate_polygon
 
 
-def main(pictureName, quality=0):
-    lineResult = 0
-    img1 = cv2.imread(pictureName)
-    img = cv2.imread(pictureName, 0)
+def main(pictureName, quality=3):
+    SIZE_FACTOR = 50
+    img = cv2.imread(pictureName, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.flip(img, -1)
     if img is None:
         print("Could not read input image")
         return
-    scale_percent = 100  # percent of original size
-    #fix the image if it is too small
+    scale_percent = 100
+    # Resize if the image is too small
     if img.shape[0] < 800:
         scale_percent = 200
     if img.shape[0] < 400:
@@ -26,61 +26,66 @@ def main(pictureName, quality=0):
     height = int(img.shape[0] * scale_percent / 100)
     dim = (width, height)
 
-    # resize image
+    # Resize and preprocess
     img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-    img1 = cv2.resize(img1, dim, interpolation=cv2.INTER_AREA)
-    img = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv.THRESH_BINARY, 11, 0)
-    img1 = cv2.flip(img1, -1)
-    img1 = cv2.rotate(img1, cv2.ROTATE_90_COUNTERCLOCKWISE)
-    img = cv2.flip(img, 0)
-    contours = find_contours(img, 4)
-    lineResult = np.zeros(img.shape + (3,), np.uint8)
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-    color = []
+    img = cv2.resize(img, (img.shape[1] * SIZE_FACTOR//100, img.shape[0]
+                     * SIZE_FACTOR//100), interpolation=cv2.INTER_AREA)
+    img = cv2.flip(img, -1)
+    img = cv2.flip(img, 1)
+    img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #gray_img1 = cv2.GaussianBlur(gray_img, (quality, quality), 0)
+    gray_img = cv2.medianBlur(gray_img, quality)
+    gray_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
+    gray_img = cv2.bitwise_not(gray_img)
+    display_image = cv2.flip(gray_img, 1)
+    display_image = cv2.flip(display_image, -1)
+    display_image = cv2.rotate(display_image, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imshow("display_image", display_image)
+    cv2.waitKey(0)
+    # Find contours using OpenCV
+    contours, _ = cv2.findContours(gray_img, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
+
+    line_segments = []
     for con in tqdm(contours):
-        cv_contour = []
-        for point in con:
-            intify = [int(point[0]), int(point[1])]
-            cv_contour.append([intify])
-        cv_contour = np.array(cv_contour)
-        color.append(findColor(img1, [cv_contour.astype(int)]))
-    iterationCount = 0
-    points = []
-    for contour in tqdm(contours):
-        # change tolerance value to control quality
-        simplifiedLine = approximate_polygon(contour, tolerance=quality)
-        simplifiedLine = simplifiedLine.astype(np.int64).tolist()
-        for index, coords in enumerate(simplifiedLine[:-1]):
-            y1, x1, y2, x2 = coords + simplifiedLine[index + 1]
-            if (x1 == x2 and y1 == y2):
+
+        # Ensure the contour has multiple points before simplifying
+        if len(con) > 1:
+            simplified_contour = con[:, 0, :]
+            if len(simplified_contour) < 2:
                 continue
-            points.append(((y1 - 400), (x1 - 400), (y2 - 400), (x2 - 400), color[iterationCount]))
-            lineResult = cv2.line(lineResult, (x1, y1),
-                                  (x2, y2), color[iterationCount])
-        iterationCount = iterationCount + 1
-    save(points)
-    #lineResult = cv2.flip(lineResult, 0)
-    #lineResult = cv2.cvtColor(lineResult, cv2.COLOR_BGR2RGB)
-    #uncomment to see preview of the image
-    #cv2.imshow("title",  lineResult)
-    #cv2.waitKey()
+            current_color = findColor(img, con)
+            color = current_color
+
+            for index in range(len(simplified_contour) - 1):
+                y1, x1 = simplified_contour[index]
+                y2, x2 = simplified_contour[index + 1]
+
+                if x1 == x2 and y1 == y2:
+                    continue
+                line_segments.append(((x1, y1), (x2, y2), color))
+
+    lineResult = np.zeros((img.shape[1], img.shape[0], img.shape[2]), np.uint8)
+    for segment in line_segments:
+        r, g, b = segment[2]
+        color = (b, g, r)
+        cv2.line(lineResult, segment[0], segment[1], color)
+    cv2.imshow("lineResult", lineResult)
+    cv2.waitKey(0)
+
+    save(line_segments)
 
 
 def save(saved_points):
-    saved_points = np.array(saved_points, dtype=object)
-    np.save('polygonStorage/polygonalPoints.npy',
-            saved_points)
+    np.save('polygonStorage/polygonalPoints.npy', np.array(saved_points, dtype=object))
 
 
-def findColor(image, c):
+def findColor(image, contour):
     mask = np.zeros(image.shape[:2], dtype="uint8")
-    cv2.drawContours(mask, c, -1, 255, -1)
-    mean = cv2.mean(image, mask=mask)[:3]
-    tupint = tuple(int(x) for x in mean)
-    return tupint
+    cv2.drawContours(mask, contour, -1, 255, -1)
+    return tuple(int(x) for x in cv2.mean(image, mask=mask)[:3])
 
 
 if __name__ == "__main__":
-    main("base_image/abbey_road.jpg")
-
+    main("base_image/image.jpg")
